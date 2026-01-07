37b109215210b6769397a4f4bbe449d487996a2c
Sk3542-git <sessomane392@gmail.com>
2026-01-07 17:16:33 +0000

    Add files via upload

---
+++ L2_A2_variables.py
@@ -0,0 +1,25 @@
+#Write a program to create different variables to store various values.
+
+# Integer variable
+
+age = 25
+
+# String variable
+
+name = "Zeeshan Hawaldar"
+
+# Display the values of the variables
+
+print(name)
+
+print("Age:", age)
+
+codingal = "codingal"
+
+print(codingal)
+
+# User Input
+
+name = input("enter your name: ")
+
+print("\n Hello", name, "\n welcome to codingal")
