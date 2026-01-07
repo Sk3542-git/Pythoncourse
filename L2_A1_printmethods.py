eaea78b015ccbc55bc515b4835845a027fbd10aa
Sk3542-git <sessomane392@gmail.com>
2026-01-07 17:16:18 +0000

    Add files via upload

---
+++ L2_A1_printmethods.py
@@ -0,0 +1,30 @@
+#Print a Sentence
+
+print("Welcome to the world of programming!")
+
+#Print a Number
+
+print(10.0)
+
+#Next Line after print
+
+print("Hello Python \'n")
+
+#Pass multiple arguments for printing
+
+print("hello",89)
+
+#End argument of Print Statement
+
+print("welcome to ", end= "\n *************************\n")
+
+#Print with variables fstring
+
+print(f"Hello, I am learning Python version {81+9}")
+
+x=150
+
+print(f"The value of x is {x}")
+
+
+
