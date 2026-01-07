7c7a16d3658efa9423244ecb6094bc12715fe09f
Sk3542-git <sessomane392@gmail.com>
2026-01-07 17:32:46 +0000

    Add files via upload
    This is my homework 
    Thanks

---
+++ Acp_birthdays.py
@@ -0,0 +1,11 @@
+Sesostris_birthdays='2013-05-14'
+Mum_birthdays='1965-02-14'
+Dad_birthdays='1963-10-31'
+Aunite_birthdays='1999-06-25'
+Sister_birthdays='2000-04-21'
+
+print("Sesostris birthday is on", Sesostris_birthdays)
+print("Mum birthday is on", Mum_birthdays)
+print("Dad birthday is on", Dad_birthdays)
+print("Aunite birthday is on", Aunite_birthdays)
+print("Sister birthday is on", Sister_birthdays)
