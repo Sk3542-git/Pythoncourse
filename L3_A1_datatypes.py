83a4bed0d5b526aeedf2b7c157fd23e437e9c113
Sk3542-git <sessomane392@gmail.com>
2026-01-09 17:35:46 +0000

    Add files via upload

---
+++ L3_A1_datatypes.py
@@ -0,0 +1,10 @@
+x=10
+print('The data type of x is', type(x),'the value of x is',x)
+b=2/5
+print('The data type of x is', type(b),'the value of x is',b)
+name='Sesostris'
+print('The data type of x is', type(name),'the value of x is',name)
+test=False
+print('The data type of x is', type(test),'the value of x is',test)
+test2=True
+print('The data type of x is', type(test2),'the value of x is',test2)
