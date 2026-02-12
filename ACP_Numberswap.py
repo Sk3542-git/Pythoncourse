50e599f4ddb4a99c978bddc7996476a38c04338c
Sk3542-git <sessomane392@gmail.com>
2026-02-12 21:22:51 +0000

    Add files via upload
    This is also another overdue hometask

---
+++ ACP_Numberswap.py
@@ -0,0 +1,16 @@
+a = input("Enter first value: ")
+b = input("Enter second value: ")
+c = input("Enter third value: ")
+
+print("\nBefore swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+
+a, b, c = b, c, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
