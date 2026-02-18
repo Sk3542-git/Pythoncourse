bfbed6e5858689568c3648ce63f5c2335eb54887
Sk3542-git <sessomane392@gmail.com>
2026-02-18 16:27:04 +0000

    Add files via upload
    This is the classwork

---
+++ ACP_powercalulator.py
@@ -0,0 +1,8 @@
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent: "))
+
+
+result = base ** exponent
+
+
+print(f"\nResult: {base} raised to the power of {exponent} is {result}")
